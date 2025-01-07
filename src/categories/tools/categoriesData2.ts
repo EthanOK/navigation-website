f5dc04c619fe4400a05c07bff8797ef73002b95f
DJ.Zhang <dengji@evg.co>
2025-01-07 15:42:37 +0800

    fix: add BlockChain Explorer

--- src/categories/tools/categoriesData2.ts
+++ src/categories/tools/categoriesData2.ts
@@ -12,10 +12,75 @@ export const categoriesData2: Category = {
       link: "https://etherscan.io",
     },
     {
-      name: "Suno V4 AI音乐创作",
-      description: "Suno V4升级，AI助力歌词创作！",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#",
+      name: "Etherscan Sepolia",
+      description:
+        "The Ethereum Blockchain Explorer",
+      icon: "/assets/images/etherscan-logo.svg",
+      link: "https://sepolia.etherscan.io",
+    },
+    {
+      name: "Basescan",
+      description:
+        "The Base(ETH) Blockchain Explorer",
+      icon: "/assets/images/etherscan-logo.svg",
+      link: "https://basescan.org",
+    },
+    {
+      name: "Basescan Sepolia",
+      description:
+        "The Base(ETH) Blockchain Explorer",
+      icon: "/assets/images/etherscan-logo.svg",
+      link: "https://sepolia.basescan.org",
+    },
+    {
+      name: "Bscscan",
+      description:
+        "The BNB Smart Chain Explorer",
+      icon: "/assets/images/bscscan-log.svg",
+      link: "https://bscscan.com",
+    },
+    {
+      name: "Bscscan Testnet",
+      description:
+        "The Testnet BNB Smart Chain Explorer",
+      icon: "/assets/images/bscscan-log.svg",
+      link: "https://testnet.bscscan.com",
+    },
+    {
+      name: "OPBNB Bscscan",
+      description:
+        "The  OPBNB Chain Explorer",
+      icon: "/assets/images/bscscan-log.svg",
+      link: "https://opbnb.bscscan.com/",
+    },
+    {
+      name: "Blockscan",
+      description:
+        "Blockscan Multichain Explorer Portfolio Analytics",
+      icon: "/assets/images/blockscan-logo.svg",
+      link: "https://blockscan.com",
+    },
+    {
+      name: "Solscan",
+      description:
+        "The Scanning Tool for the Solana Ecosystem",
+      icon: "https://avatars.githubusercontent.com/u/92743431",
+      link: "https://solscan.io",
+    },
+    {
+      name: "Solana Explorer",
+      description:
+        "Inspect transactions on the Solana blockchain.", 
+      icon: "https://explorer.solana.com/favicon.svg",
+      link: "https://explorer.solana.com",
+    },
+    {
+      name: "Solana FM",
+      description:
+        "Next-Gen Solana Explorer",
+      icon: "/assets/images/solanafm-logo.svg",
+      link: "https://solana.fm",
+
     },
     {
       name: "AI写作助手",
