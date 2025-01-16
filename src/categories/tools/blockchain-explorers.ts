f4f90f9dc82f885dda69ebd54060438a6db614ca
EthanZhang <sdzdj01@163.com>
2025-01-16 19:10:31 +0800

    feat: add Sealos

--- src/categories/tools/blockchain-explorers.ts
+++ src/categories/tools/blockchain-explorers.ts
@@ -70,43 +70,6 @@ const blockchainExplorers: Category = {
       description: "Next-Gen Solana Explorer",
       icon: "/assets/images/solanafm-logo.svg",
       link: "https://solana.fm"
-    },
-    {
-      name: "AI写作助手",
-      description: "一站式AI自媒体创作平台，支持GPT4、Claude3、Midjourney等模",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "稿定设计",
-      description: "AI灵感绘图、AI智能设计、AI文生图、AI取标题，创作效率新FUN",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "创一AI播客生成神器",
-      description: "一站式内容创意平台，高质量视频分析，脚本创作、播客创作",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "比格AI PPT",
-      description:
-        "支持AI一键生成PPT大纲，导入本地大纲文件，一键更换模版配色。",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "有道",
-      description: "一站式AI创作工具，1200+免费3D数字人。",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "快影3D秀",
-      description: "简单高效的AI产品动画制作平台",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
     }
   ]
 };
--- src/categories/tools/rpc-services.ts
+++ src/categories/tools/rpc-services.ts
@@ -33,44 +33,6 @@ const rpcServices: Category = {
       description: "1RPC | Web3 Private RPC Relay",
       icon: "https://www.1rpc.io/favicon.webp",
       link: "https://www.1rpc.io"
-    },
-    {
-      name: "通义智文",
-      description: "论文/文献/报告/网页。AI帮你读懂更多，读得更快，读得更好",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "云知写作",
-      description: "一款专业的论文编辑工具，基于先进的语义分析，智能AI论文助手",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "PaperFree",
-      description: "一款全面高效的论文查重系统，致力于为学者和学术界提供更英文",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "智绘AI/学术助手",
-      description: "智绘AI的一个特点，支持文字、表格、颜色、学术翻译以及论文",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "百度橙篇AI",
-      description:
-        "一个综合性的AI应用商店，主要用于处理和梳理大量、多格式、超长",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
-    },
-    {
-      name: "青柠学术",
-      description:
-        "首个学术创作平台，由学术大咖出品，通过国内人文社科领域的精心",
-      icon: "https://static.kaolamedia.com/4b/61/4b6111133a693079ab67704e45df3cff/icon",
-      link: "#"
     }
   ]
 };
